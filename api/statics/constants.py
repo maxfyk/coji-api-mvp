bd32884f876b5e78bbc7189eae8956c1edf965b4
Max <maxfyk@gmail.com>
2022-10-25 16:42:42 +0800

    Update constants.py

--- api/statics/constants.py
+++ api/statics/constants.py
@@ -5,7 +5,8 @@ import os
 COJI_DATA_TYPES = ('text', 'url', 'file')  # 'video', 'file', 'ar'
 COJI_CREATE_REQUEST_KEYS = ('in-data', 'data-type', 'user-id', 'style-info', 'location')
 COJI_UPDATE_REQUEST_KEYS = ('in-data', 'data-type', 'user-id', 'code-id', 'style-info', 'location')
-COJI_DB_CODE_FIELDS = ('index', 'in-data', 'data-type', 'user-id', 'style-info', 'time-created', 'time-updated')
+COJI_DB_CODE_FIELDS = (
+'index', 'in-data', 'data-type', 'user-id', 'style-info', 'time-created', 'time-updated', 'location')
 COJI_STYLE_NAMES = ('geom-original',)
 RESPONSE_DECODE_ERROR_DICTS = {'image': 'Blurry photo', 'keyboard': 'Wrong code'}
 # coji-decode
