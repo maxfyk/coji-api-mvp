08c00fb972397e9d398092f648c9afd7a9a1f947
Max <maxfyk@gmail.com>
2022-11-01 16:45:13 +0800

    stats_logger.registry

--- api/services/prometheus_metrics.py
+++ api/services/prometheus_metrics.py
@@ -4,7 +4,7 @@ os.environ['prometheus_multiproc_dir'] = '/app/prometheus_metrics'
 from modules import stats_logger
 from flask import Blueprint
 from flask import Response
-from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest, multiprocess
 
 prometheus_metrics = Blueprint('prometheus-metrics', __name__)
 
@@ -13,6 +13,7 @@ prometheus_metrics = Blueprint('prometheus-metrics', __name__)
 def prometheus_metrics_get():
     """Scrape metrics"""
     try:
+        multiprocess.MultiProcessCollector(stats_logger.registry)
         resp = Response(generate_latest(stats_logger.registry), mimetype=CONTENT_TYPE_LATEST)
         stats_logger.get_new_decode_request_counter()
         return resp
