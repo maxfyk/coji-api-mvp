ab39ed2660b995bb63de9107e370637d3504d40b
Max <maxfyk@gmail.com>
2022-11-29 17:39:12 +0800

    Update db_operations.py

--- api/modules/db_operations.py
+++ api/modules/db_operations.py
@@ -89,5 +89,6 @@ def update_feedback(code_id, feedback, db_root=None):
     if 'likes' not in full_stats.keys():
         full_stats.update(new_stats)
     full_stats[feedback] += 1
+    print(full_stats)
     update_code(code_id, full_stats, db_root)
     return True
